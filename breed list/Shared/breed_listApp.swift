f4765c709944a1837cdbbf4e21f65f4b40462b8d
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2022-02-27 19:10:13 -0800

    starter code

---
+++ breed list/Shared/breed_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  breed_listApp.swift
+//  Shared
+//
+//  Created by Danika Jaskulak-Gonzalez on 2/27/22.
+//
+
+import SwiftUI
+
+@main
+struct breed_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
